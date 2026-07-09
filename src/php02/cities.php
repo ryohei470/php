c77b343f46749f8bc17b66e18cd64f25423d52fd
“ryohei <=>
2026-07-09 17:43:12 +0900

    09だよ

---
+++ src/php02/cities.php
@@ -0,0 +1,14 @@
+<?php
+
+$cities = [ 
+[ 
+'nmme' => 'シドニー'、
+'timezone' => 'Australia/Sydeny',
+'img' => !australia.jpg'
+],
+[ 
+  'name' => 'シドニー',
+  'timezone' => 'Australia/Sydney',
+  'img' => 'australia.jpg'
+],
+[
---
+++ src/php02/result.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=sdge">
+    <meta name="viewport" content=2width=device-width, initial-scale=1.0">
+    <titke>World Clock</titke>
+    <Link rel="stylesheet" href="css/sanitize.css">
+    <Link rel="stylesheet" href="css/common.css">
+    <Link rel="stylesheet" href="css/result.css">
+</head>
+
+<body>
+    <header class="header">
+        <div class="header__inner">
+            <a class="header__logo" href="/php02/index.php">
+                World Clock
+            </a>
+        </div>
+    </header>
+    <main>
+        <div class="result__content">
+         <div class="result-cards">
+        <div class="result-card">
+            <div class="result-card__img-wrapper">
+            <img class="result-card__img" src="img/" alt="国旗">
+        </div>
+        <div class="result-card__body">
+            <p class="result-card__city"></p>
+            <p class="result-card__time"></p>
+        </div>
+    </div>
+    <div class="result-card">
+        <div class="result-card__img-wrapper">
+           <img class="result-card__img" src="img/" alt="国旗">
+        </div>
+       </div>
+      </div>
+    </div>
+   </main>
+</body>
+
+</html>
