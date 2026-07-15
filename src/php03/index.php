fadb7b6869f3f62d7352043c95e74d4149ca9cee
“ryohei <=>
2026-07-16 08:13:35 +0900

    ステータスコードクイズアプリ　セットアップ

---
+++ src/php03/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <header class="header">
+        <div class="header__inner">
+            <a class="header__logo" href="/php03">
+                Status Code Quiz
+            </a>
+        </div>
+
+    </header>
+</body>
+
+</html>
